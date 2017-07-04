04d84a3430cfcf688b660de025327bbced8c29d0
ahorod <prozerpina112009@gmail.com>
2017-07-03 22:16:20 -0700

    add scopes

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,16 @@
 class Product < ApplicationRecord
   validates :name, :cost, :origin, :presence => true
   has_many :reviews
+
+  scope :most_reviews, -> {(
+   select("products.name, products.id, count(reviews.id) as reviews_count")
+   .joins(:reviews)
+   .group("products.id")
+   .order("reviews_count DESC")
+   .limit(5)
+   )}
+
+   scope :local, -> {where("origin=?",  "Ukraine")}
+
+   scope :most_recent, -> { order(created_at: :desc).limit(3)}
 end
