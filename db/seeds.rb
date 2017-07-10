fbfef587f1b0315e0fbd3010e25a5a78d8c3e82d
ahorod <prozerpina112009@gmail.com>
2017-07-09 23:03:04 -0700

    add faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.destroy_all
+Review.destroy_all
+
+50.times do |index|
+  new_product = Product.create!(
+    name: Faker::Coffee.blend_name,
+    cost: Faker::Number.between(1, 100),
+    origin: Faker::Address.country,
+  )
+  rand(1..5).times do |review|
+    Review.create!(
+      author: Faker::Friends.character,
+      rating: Faker::Number.between(1, 5),
+      content: Faker::Lorem.sentence(15),
+      product_id: new_product.id
+    )
+  end
+end
